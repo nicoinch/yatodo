00e67a434ec5673cd16d3aadd8503b0fb51eb4a0
Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
2022-02-08 12:27:26 +0100

    :alembic: Try radix ui avatar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Avatar from '@radix-ui/react-avatar';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +8,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Avatar.Root>
+          <Avatar.Image
+            src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+            alt="Colm Tuite"
+          />
+          <Avatar.Fallback delayMs={600}>CT</Avatar.Fallback>
+        </Avatar.Root>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
