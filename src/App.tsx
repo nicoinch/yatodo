4c85b96bfb1dc4b6d5325440f853d5206c7188e9
Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
2022-02-10 08:36:00 +0100

    :lipstick: Style radix component with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <Avatar.Root>
+        <Avatar.Root className="">
           <Avatar.Image
             src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
             alt="Colm Tuite"
